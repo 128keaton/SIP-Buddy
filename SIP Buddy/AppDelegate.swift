c997d6c58e988d3a8ac3c7f1862f2fc39f63b979
Keaton Burleson <keaton.burleson@me.com>
2020-06-20 00:03:22 -0500

    Initial Commit

---
+++ SIP Buddy/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SIP Buddy
+//
+//  Created by Keaton Burleson on 6/20/20.
+//  Copyright © 2020 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SIP Buddy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SIP Buddy
+//
+//  Created by Keaton Burleson on 6/20/20.
+//  Copyright © 2020 Keaton Burleson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
